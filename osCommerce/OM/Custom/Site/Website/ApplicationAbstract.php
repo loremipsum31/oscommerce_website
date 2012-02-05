72cd585f63f6e0afb7a184958fedcc5a9a155541
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-02-05 15:50:12 +0100

    Simplify ApplicationAbstract

--- osCommerce/OM/Custom/Site/Website/ApplicationAbstract.php
+++ osCommerce/OM/Custom/Site/Website/ApplicationAbstract.php
@@ -2,67 +2,13 @@
 /**
  * osCommerce Website
  * 
- * @copyright Copyright (c) 2011 osCommerce; http://www.oscommerce.com
+ * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
  * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
  */
 
   namespace osCommerce\OM\Core\Site\Website;
 
-  use osCommerce\OM\Core\HTML;
-  use osCommerce\OM\Core\OSCOM;
-
   abstract class ApplicationAbstract extends \osCommerce\OM\Core\ApplicationAbstract {
-    public function __construct($process = true) {
-      $this->initialize();
-
-      if ( $process === true ) {
-        $action = null;
-        $action_index = 1;
-
-        if ( count($_GET) > 1 ) {
-          $requested_action = HTML::sanitize(basename(key(array_slice($_GET, 1, 1, true))));
-
-          if ( $requested_action == OSCOM::getSiteApplication() ) {
-            $requested_action = null;
-
-            if ( count($_GET) > 2 ) {
-              $requested_action = HTML::sanitize(basename(key(array_slice($_GET, 2, 1, true))));
-
-              $action_index = 2;
-            }
-          }
-
-          if ( !empty($requested_action) && self::siteApplicationActionExists($requested_action) ) {
-            $action = $requested_action;
-          }
-        }
-
-        if ( isset($action) ) {
-          call_user_func(array('osCommerce\\OM\\Core\\Site\\Website\\Application\\' . OSCOM::getSiteApplication() . '\\Action\\' . $action, 'execute'), $this);
-
-          $action_index++;
-
-          if ( $action_index < count($_GET) ) {
-            $action = array($action);
-
-            for ( $i = $action_index, $n = count($_GET); $i < $n; $i++ ) {
-              $subaction = HTML::sanitize(basename(key(array_slice($_GET, $i, 1, true))));
-
-              if ( self::siteApplicationActionExists(implode('\\', $action) . '\\' . $subaction) ) {
-                call_user_func(array('osCommerce\\OM\\Core\\Site\\Website\\Application\\' . OSCOM::getSiteApplication() . '\\Action\\' . implode('\\', $action) . '\\' . $subaction, 'execute'), $this);
-
-                $action[] = $subaction;
-              } else {
-                break;
-              }
-            }
-          }
-        }
-      }
-    }
-
-    public function siteApplicationActionExists($action) {
-      return class_exists('osCommerce\\OM\\Core\\Site\\Website\\Application\\' . OSCOM::getSiteApplication() . '\\Action\\' . $action);
-    }
+    abstract protected function initialize();
   }
 ?>
