1375bb09e0db0a77c7c13a0d53948e357fbc8430
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-10-18 13:58:42 +0200

    Rename default template to Amy

---
+++ osCommerce/OM/Custom/Site/Website/Template/Amy/Controller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * osCommerce Online Merchant
+ * 
+ * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
+ * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
+ */
+
+  namespace osCommerce\OM\Core\Site\Website\Template\Amy;
+
+  class Controller extends \osCommerce\OM\Core\TemplateAbstract {
+  }
+?>
