e6e5aeb9f84a9f2b690a0d4404cdc6515c8d1782
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-10-19 03:49:17 +0200

    Replace static layout with dynamic layout

--- osCommerce/OM/Custom/Site/Website/Application/Services/Controller.php
+++ osCommerce/OM/Custom/Site/Website/Application/Services/Controller.php
@@ -8,12 +8,66 @@
 
   namespace osCommerce\OM\Core\Site\Website\Application\Services;
 
+  use osCommerce\OM\Core\HTML;
   use osCommerce\OM\Core\OSCOM;
+  use osCommerce\OM\Core\Registry;
 
   class Controller extends \osCommerce\OM\Core\Site\Website\ApplicationAbstract {
     protected function initialize() {
-      $this->_page_contents = 'main.html';
-      $this->_page_title = OSCOM::getDef('services_html_page_title');
+      $OSCOM_Template = Registry::get('Template');
+
+      $OSCOM_Template->setValue('services_action', '');
+
+      $action = null;
+      $action_index = 1;
+      $subaction = null;
+
+      if ( count($_GET) > 1 ) {
+        $requested_action = HTML::sanitize(basename(key(array_slice($_GET, 1, 1, true))));
+
+        if ( $requested_action == OSCOM::getSiteApplication() ) {
+          $requested_action = null;
+
+          if ( count($_GET) > 2 ) {
+            $requested_action = HTML::sanitize(basename(key(array_slice($_GET, 2, 1, true))));
+
+            $action_index = 2;
+          }
+        }
+
+        if ( !empty($requested_action) && in_array($requested_action, static::getServiceCategories()) ) {
+          $action = $requested_action;
+        }
+      }
+
+      if ( isset($action) ) {
+        $action_index++;
+
+        if ( $action_index < count($_GET) ) {
+          $subaction = HTML::sanitize(basename(key(array_slice($_GET, $action_index, 1, true))));
+        }
+
+        if ( in_array($subaction, static::getServicePartners($action)) ) {
+          $this->_page_contents = 'info.html';
+          $this->page_title = OSCOM::getDef('info_html_page_title');
+        } else {
+          $this->_page_contents = 'list.html';
+          $this->page_title = OSCOM::getDef(strtolower($action) . '_html_page_title');
+        }
+
+        $OSCOM_Template->setValue('services_action', $action, true);
+      } else {
+        $this->_page_contents = 'main.html';
+        $this->_page_title = OSCOM::getDef('services_html_page_title');
+      }
+    }
+
+    protected static function getServiceCategories() {
+      return array('Hosting', 'Developers', 'Payment', 'Templates', 'Security', 'Marketing');
+    }
+
+    protected static function getServicePartners($category) {
+      return array('Info');
     }
   }
 ?>
